3761c2c6c41457c152cfe23904f052aea563a0ae
Victor O. Costa <victor-o.c@hotmail.com>
2019-11-14 17:55:49 -0300

    Finish shapelet normalization implementation (untested) TODO: selection between pivot and target shapelets

--- hardware/shapelet_distance.vhd
+++ hardware/shapelet_distance.vhd
@@ -73,6 +73,10 @@ architecture behavioral of shapelet_distance is
     signal norm_selector_s                              : std_logic;
     -- Processed points counter register                        
     signal reg_norm_count_s                             : natural range 3 to MAX_LEN;
+    -- reg_norm_count_s incremented by NUM_PU
+    signal norm_count_inc_s                             : natural range 3 to MAX_LEN;
+    -- Normalization outputs
+    signal norm_out_s                                   : pu_operands_t;
     
     
     ---- SHAPELET DISTANCE DEFINITIONS
@@ -89,6 +93,7 @@ architecture behavioral of shapelet_distance is
     
     -- Array of accumulator registers (combinational)
     signal reg_accumulators_s                           : pu_operands_t;
+    signal accumulators_en_s                            : std_logic;
     
     -- addition/subtraction signals
     signal add_or_sub_s                                 : pu_flags_t;   -- 0: add, 1: sub
@@ -123,10 +128,14 @@ architecture behavioral of shapelet_distance is
     -- 
     type pu_matrix_t is array(0 to NUM_PU-1) of positions_by_pu_t;
     
+    -- Shapelet selector, 0: pivot, 1:target
+    signal shapelet_sel_s                               : std_logic;
     -- Input buffer may be either pivot or target shapelets
     signal input_buffer_s                               : shapelet_buffer_t;
-    -- Select which term will be presented to the PUs
-    signal pu_sel_s                                     : natural range 0 to LEN_MAX/NUM_PU;
+    -- Register to count how many blocks of shapelet positions were already presented to the PUs, acting as a MUX selector for the shapelet . (reg_norm_count_s is an absolute element count)
+    signal reg_block_sel_s                                 : natural range 0 to LEN_MAX/NUM_PU;
+    signal block_sel_rst_s                                 : std_logic;
+    signal block_sel_inc_s                                 : std_logic;
     -- Matrix with processing units as rows and shapelet positions for each PU as columns
     signal output_matrix_s                              : pu_matrix_t;
     -- The shapelet positions presented to each of the NUM_PU processing elements
@@ -135,147 +144,6 @@ architecture behavioral of shapelet_distance is
 begin
     -- Decrement reg_shapelet_length_s
     dec_length_s <= reg_shapelet_length_s - 1;
-    
-    ---- MUX to present shapelet positions to the right Processing Units
-    -- Selects which shapelet is presented to the MUX
-    input_buffer_s <=   buffer_pivot_s  when ? else
-                        buffer_target_s;
-    --
-    OUTER: for i in NUM_PU - 1 downto 0 generate
-        -- 
-        INNER: for j in (LEN_MAX/NUM_PU - 1) downto 0 generate
-            -- 
-            output_matrix_s(i)(j) <= input_buffer_s(i + j*NUM_PU);
-        end generate INNER;
-        
-    end generate OUTER;
-
-    -- 
-    MUX: for i in NUM_PU - 1 downto 0 generate
-        shapelet_elements_mux_s(i) <= output_matrix_s(i)(pu_sel_s);
-    end generate MUX;
-    
-    
-    -- Processing units combinational logic
-    
-    
-    -- Generate processing units (adder/subtractor, multiplier, divider, square root)
-    PROCESSING_UNITS: for i in 0 to NUM_PU - 1 generate
-        
-        
-         -- Addsub 
-        add_or_sub_s(i) <= '0'                      when reg_norm_state_s = Ssum and reg_buf_state_s = Sbegin else '1';
-        addsub_opa_s(i) <= reg_accumulators_s(i)    when reg_norm_state_s = Ssum and reg_buf_state_s = Sbegin else div_out_s;
-        addsub_opb_s(i) <= mul_out_s(i)             when reg_norm_state_s = Ssum and reg_buf_state_s = Sbegin else ?;
-        -- ADDSUB computes in 6 cycles
-        addsub: fp_addsub
-        port map(
-            clk_i 			=> clk,      
-            op_type         => add_or_sub_s(i),                    -- 0 = add, 1 = sub
-            opa_i        	=> addsub_opa_s(i),
-            opb_i           => addsub_opb_s(i),
-            output_o        => addsub_out_s(i),
-            -- Exceptions
-            ine_o 			=> open,                -- inexact
-            overflow_o  	=> open,                -- overflow
-            underflow_o 	=> open,                -- underflow
-            inf_o			=> open,                -- infinity
-            zero_o			=> open,                -- zero
-            qnan_o			=> open,                -- queit Not-a-Number
-            snan_o			=> open                 -- signaling Not-a-Number
-        );
-        
-        -- Multiplier 
-        mul_start_s(i) <= '1' when reg_norm_state_s = Ssquare and reg_buf_state_s = Sbegin else '0';
-        -- Square shapelet elements in normalization and the difference in euclidean distance calculation
-        mul_opa_s(i) <= shapelet_elements_mux_s(i) when reg_norm_state_s = Ssquare and reg_buf_state_s = Sbegin else addsub_out_s(i);
-        mul_opb_s(i) <= shapelet_elements_mux_s(i) when reg_norm_state_s = Ssquare and reg_buf_state_s = Sbegin else addsub_out_s(i);
-        
-        -- MUL computes in 11 cycles
-        mul: fp_mul
-        port map(
-            clk_i 			=> clk,
-            start_i         => mul_start_s(i),
-            opa_i        	=> mul_opa_s(i),                    -- Input Operands A & B
-            opb_i           => mul_opb_s(i),
-            output_o        => mul_out_s(i),
-            -- Exceptions
-            ine_o 			=> open,                -- inexact
-            overflow_o  	=> open,                -- overflow
-            underflow_o 	=> open,                -- underflow
-            inf_o			=> open,                -- infinity
-            zero_o			=> open,                -- zero
-            qnan_o			=> open,                -- queit Not-a-Number
-            snan_o			=> open                 -- signaling Not-a-Number
-        );
-        
-        -- Divisor 
-        
-        
-        -- Div computes in 33 cycles (says fpu code)
-        div: fp_div
-        port map(
-            clk_i 			=> clk,
-            start_i         => div_start_s(i),
-            opa_i        	=> div_opa_s(i),                    -- Input Operands A & B
-            opb_i           => div_opb_s(i),
-            output_o        => div_out_s(i),
-            
-            -- Exceptions
-            ine_o 			 => open,                       -- inexact
-            overflow_o  	 => open,                       -- overflow
-            underflow_o 	 => open,                       -- underflow
-            div_zero_o       => open,                       -- division by zero
-            inf_o			 => open,                       -- infinity
-            zero_o			 => open,                       -- zero
-            qnan_o			 => open,                       -- queit Not-a-Number
-            snan_o			 => open                        -- signaling Not-a-Number
-        );
-    end generate PROC_ELEMENTS; 
-    
-    
-    -- FUTURE: ADDER TREE
-    -- Sum accumulators (NOW: assumes there are only 2 PUs)
-    acc_sum_opa_s <= reg_accumulators_s(0);
-    acc_sum_opb_s <= reg_accumulators_s(1);
-    
-    -- sums accumulator registers
-    sum_accs: fp_addsub
-    port map(
-        clk_i 			=> clk,      
-        op_type         => '0',                    -- 0 = add, 1 = sub
-        opa_i        	=> acc_sum_opa_s,
-        opb_i           => acc_sum_opb_s,
-        output_o        => acc_sum_out_s,
-        -- Exceptions
-        ine_o 			=> open,                -- inexact
-        overflow_o  	=> open,                -- overflow
-        underflow_o 	=> open,                -- underflow
-        inf_o			=> open,                -- infinity
-        zero_o			=> open,                -- zero
-        qnan_o			=> open,                -- queit Not-a-Number
-        snan_o			=> open                 -- signaling Not-a-Number
-    );
-    
-    sqrt_op <= acc_sum_out_s;
-    
-    -- Sqrt computes in 33 cycles (outside for .. generate)
-    sqrt: fp_sqrt
-    port map(
-        clk_i 			=> clk,
-        start_i         => sqrt_start_s,
-        opa_i        	=> sqrt_op,
-        output_o        => sqrt_out_s,
-        -- Exceptions
-        ine_o 			=> open,                            -- inexact
-        overflow_o  	=> open,                            -- overflow
-        underflow_o 	=> open,                            -- underflow
-        inf_o			=> open,                            -- infinity
-        zero_o			=> open,                            -- zero
-        qnan_o			=> open,                            -- queit Not-a-Number
-        snan_o			=> open                             -- signaling Not-a-Number
-    );
-    
     
     -- Buffer filling combinational logic
     buffer_ready_s <= '1' when reg_buf_state_s = Send else '0';
@@ -347,8 +215,189 @@ begin
     end process;
     
     
+    ---- MUX to present shapelet positions to the right Processing Units
+    -- Selec which shapelet will go under the mux generated below
+    shapelet_sel_s <=   '0' when .. else '1';
+    
+    -- Selects which shapelet is presented to the MUX
+    input_buffer_s <=   buffer_pivot_s  when shapelet_sel_s = '0' else
+                        buffer_target_s;
+    --
+    OUTER: for i in NUM_PU - 1 downto 0 generate
+        -- 
+        INNER: for j in (LEN_MAX/NUM_PU - 1) downto 0 generate
+            -- 
+            output_matrix_s(i)(j) <= input_buffer_s(i + j*NUM_PU);
+        end generate INNER;
+        
+    end generate OUTER;
+
+    
+    -- Mux selector is a counter of how many blocks were processed to the moment (designed as a separate process so it can be set both in normalization and euclidean distance, do the same with reg_norm_count_s and reg_dist_count_s)
+    block_sel_rst_s <= '1' when reg_norm_state_s = Sbegin or reg_norm_state_s = Ssqrt  else '0';
+    block_sel_inc_s <= '1' when reg_norm_state_s = Sacc or reg_norm_state_s = Sout_div else '0';
+    reg_selector: process(clk, block_sel_rst_s, block_sel_inc_s)
+    begin
+        if rising_edge(clk) then
+            if block_sel_rst_s = '1' then
+                reg_block_sel_s <= 0;
+            else if block_sel_inc_s = '1' then
+                reg_block_sel_s <= reg_block_sel_s + 1;
+            end if;
+    end process;
+    -- 
+    MUX: for i in NUM_PU - 1 downto 0 generate
+        shapelet_elements_mux_s(i) <= output_matrix_s(i)(reg_block_sel_s);
+    end generate MUX;
+    
+    
+    ---- PROCESSING UNITS
+    accumulators_en_s  <= '1' when reg_norm_state_s = Sacc else '0';
+    
+    -- Generate processing units (adder/subtractor, multiplier, divider, square root)
+    PROCESSING_UNITS: for i in 0 to NUM_PU - 1 generate
+    
+    
+        -- Normalization output is division output
+        norm_out_s(i) <= div_out_s(i);
+    
+        -- Accumulator registers
+        acc_regs: process(clk, rst_n, accumulators_en_s)
+        begin
+            if rising_edge(clk) then
+                if rst_n = '0' then
+                    reg_accumulators_s(i) <= (others => (others => '0'));
+                else
+                    if accumulators_en_s = '1' then
+                        reg_accumulators_s(i) <= addsub_out_s(i);
+                    end if;
+                end if;
+        end process;
+        
+        
+         -- Addsub 
+        add_or_sub_s(i) <=  '0'                      when reg_norm_state_s = Ssum and reg_buf_state_s = Sbegin else '1';
+        addsub_opa_s(i) <=  reg_accumulators_s(i)    when reg_norm_state_s = Ssum and reg_buf_state_s = Sbegin else 
+                            div_out_s;
+        addsub_opb_s(i) <=  mul_out_s(i)             when reg_norm_state_s = Ssum and reg_buf_state_s = Sbegin else
+                            input_buffer_s;
+                            
+        -- ADDSUB computes in 6 cycles
+        addsub: fp_addsub
+        port map(
+            clk_i 			=> clk,      
+            op_type         => add_or_sub_s(i),                    -- 0 = add, 1 = sub
+            opa_i        	=> addsub_opa_s(i),
+            opb_i           => addsub_opb_s(i),
+            output_o        => addsub_out_s(i),
+            -- Exceptions
+            ine_o 			=> open,                -- inexact
+            overflow_o  	=> open,                -- overflow
+            underflow_o 	=> open,                -- underflow
+            inf_o			=> open,                -- infinity
+            zero_o			=> open,                -- zero
+            qnan_o			=> open,                -- queit Not-a-Number
+            snan_o			=> open                 -- signaling Not-a-Number
+        );
+        
+        -- Multiplier 
+        mul_start_s(i) <= '0'                       when reg_norm_state_s = Ssquare and reg_buf_state_s = Sbegin    else '1';
+        -- Square shapelet elements in normalization and the difference in euclidean distance calculation
+        mul_opa_s(i) <= shapelet_elements_mux_s(i)  when reg_norm_state_s = Ssquare and reg_buf_state_s = Sbegin    else addsub_out_s(i);
+        mul_opb_s(i) <= shapelet_elements_mux_s(i)  when reg_norm_state_s = Ssquare and reg_buf_state_s = Sbegin    else addsub_out_s(i);
+        
+        -- MUL computes in 11 cycles
+        mul: fp_mul
+        port map(
+            clk_i 			=> clk,
+            start_i         => mul_start_s(i),
+            opa_i        	=> mul_opa_s(i),                    -- Input Operands A & B
+            opb_i           => mul_opb_s(i),
+            output_o        => mul_out_s(i),
+            -- Exceptions
+            ine_o 			=> open,                -- inexact
+            overflow_o  	=> open,                -- overflow
+            underflow_o 	=> open,                -- underflow
+            inf_o			=> open,                -- infinity
+            zero_o			=> open,                -- zero
+            qnan_o			=> open,                -- queit Not-a-Number
+            snan_o			=> open                 -- signaling Not-a-Number
+        );
+        
+        -- Divisor 
+        div_start_s(i)  <= '0'                          when reg_norm_state_s = Sdiv and reg_buf_state_s = Sbegin;
+        div_opa_s(i)    <= shapelet_elements_mux_s(i);
+        div_opb_s(i)    <= sqrt_out_s;
+        
+        -- Div computes in 33 cycles (says fpu code)
+        div: fp_div
+        port map(
+            clk_i 			=> clk,
+            start_i         => div_start_s(i),
+            opa_i        	=> div_opa_s(i),                    -- Input Operands A & B
+            opb_i           => div_opb_s(i),
+            output_o        => div_out_s(i),
+            
+            -- Exceptions
+            ine_o 			 => open,                       -- inexact
+            overflow_o  	 => open,                       -- overflow
+            underflow_o 	 => open,                       -- underflow
+            div_zero_o       => open,                       -- division by zero
+            inf_o			 => open,                       -- infinity
+            zero_o			 => open,                       -- zero
+            qnan_o			 => open,                       -- queit Not-a-Number
+            snan_o			 => open                        -- signaling Not-a-Number
+        );
+    end generate PROC_ELEMENTS; 
+    
+    
+    -- FUTURE: ADDER TREE
+    -- Sum accumulators (NOW: assumes there are only 2 PUs)
+    acc_sum_opa_s <= reg_accumulators_s(0);
+    acc_sum_opb_s <= reg_accumulators_s(1);
+    
+    -- sums accumulator registers
+    sum_accs: fp_addsub
+    port map(
+        clk_i 			=> clk,      
+        op_type         => '0',                    -- 0 = add, 1 = sub
+        opa_i        	=> acc_sum_opa_s,
+        opb_i           => acc_sum_opb_s,
+        output_o        => acc_sum_out_s,
+        -- Exceptions
+        ine_o 			=> open,                -- inexact
+        overflow_o  	=> open,                -- overflow
+        underflow_o 	=> open,                -- underflow
+        inf_o			=> open,                -- infinity
+        zero_o			=> open,                -- zero
+        qnan_o			=> open,                -- queit Not-a-Number
+        snan_o			=> open                 -- signaling Not-a-Number
+    );
+    
+    -- Single SQRT unit
+    sqrt_op <= acc_sum_out_s;
+    sqrt_start_s <= '0' when reg_norm_state_s = Ssqrt and reg_buf_state_s = Sbegin else '1';
+    
+    -- Sqrt computes in 33 cycles (outside for .. generate)
+    sqrt: fp_sqrt
+    port map(
+        clk_i 			=> clk,
+        start_i         => sqrt_start_s,
+        opa_i        	=> sqrt_op,
+        output_o        => sqrt_out_s,
+        -- Exceptions
+        ine_o 			=> open,                            -- inexact
+        overflow_o  	=> open,                            -- overflow
+        underflow_o 	=> open,                            -- underflow
+        inf_o			=> open,                            -- infinity
+        zero_o			=> open,                            -- zero
+        qnan_o			=> open,                            -- queit Not-a-Number
+        snan_o			=> open                             -- signaling Not-a-Number
+    );
+    
+    
     -- Normalization combinational logic
-    reg_norm_count_inc_s <= reg_norm_count_s + NUM_PU;
+    norm_count_inc_s <= reg_norm_count_s + NUM_PU;
     norm_ready_s <= '1' when reg_norm_state_s = Send else '0';
 
     -- Normalization sequential logic
@@ -380,7 +429,7 @@ begin
                         end if;
                         
                     when Sacc =>
-                        reg_norm_count_s <= reg_norm_count_inc_s;
+                        reg_norm_count_s <= norm_count_inc_s;
                         
                         if reg_norm_count_s >= reg_shapelet_length_s then
                             reg_norm_state_s <= Ssqrt;
@@ -402,7 +451,7 @@ begin
                         end if;
                         
                     when Sout_div =>
-                        reg_norm_count_s <= reg_norm_count_inc_s;
+                        reg_norm_count_s <= norm_count_inc_s;
                         
                         if reg_norm_count_s >= reg_shapelet_length_s then
                             reg_norm_state_s <= Send;
