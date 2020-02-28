f9f9084315ea28e9b5ef5689fd97d625bb545b5e
vctrop <victor-o.c@hotmail.com>
2020-02-28 20:55:06 -0300

    Upload results from expectation-based z-score

--- software/script_vanilla_exp_znorm.sh
+++ software/script_vanilla_exp_znorm.sh
@@ -1,4 +1,4 @@
-./test_shapelets_zscore_abs_exp data/BirdChicken/BirdChicken_TRAIN.csv results_exp/bird_z_abs_exp 3 512 20
-./test_shapelets_zscore_abs_exp data/Coffee/Coffee_TRAIN.csv results_exp/coffee_z_abs_exp 3 286 20
-./test_shapelets_zscore_abs_exp data/TwoLeadECG/TwoLeadECG_TRAIN.csv results_exp/ecg_z_abs_exp 3 82 20
-./test_shapelets_zscore_abs_exp data/Wafer/Wafer_TRAIN.csv results_exp/wafer_zscore_abs_exp 3 152 20
+./extract_shapelets_zscore_abs_exp data/BirdChicken/BirdChicken_TRAIN.csv results_exp/bird_z_abs_exp 3 512 20
+./extract_shapelets_zscore_abs_exp data/Coffee/Coffee_TRAIN.csv results_exp/coffee_z_abs_exp 3 286 20
+./extract_shapelets_zscore_abs_exp data/TwoLeadECG/TwoLeadECG_TRAIN.csv results_exp/ecg_z_abs_exp 3 82 20
+./extract_shapelets_zscore_abs_exp data/Wafer/Wafer_TRAIN.csv results_exp/wafer_zscore_abs_exp 3 152 20
