c68ce58b7a5c8d5703d4a1b1e87a4285226524e7
Victor O. Costa <victor-o.c@hotmail.com>
2020-05-03 09:13:26 -0300

    (minor) forgot line in the last commit

--- software/sw_hw_integration_fstat/fstat_from_file.c
+++ software/sw_hw_integration_fstat/fstat_from_file.c
@@ -4,45 +4,45 @@
 
 
 int main(int argc, char *argv[]){
-    char *errors_filename, *dataset_filename;
-    FILE *errors_file_descriptor;
+    char *distances_filename, *dataset_filename;
+    FILE *file_descriptor;
     const uint8_t HEX_BSIZE = 8 + 1;
     char hex_buffer[HEX_BSIZE];
     Timeseries *T;
     uint16_t num_ts;
 
-    float *errors_array;
-    uint32_t uint_error;
-    float float_error;
+    float *distances_array;
+    uint32_t uint_distance;
+    float float_distance, fstat;
     
     // Get filenames and k from argv
     if(argc != 3){
-        printf("Please use: %s {errors_filename} {used_dataset_filename}\n", argv[0]);
+        printf("Please use: %s {distances_filename} {used_dataset_filename}\n", argv[0]);
         exit(-1);
     }
     
-    // Get number of time-series (same as number of errors) and errors filename from terminal arguments
-    errors_filename = argv[1];
+    // Get number of time-series (same as number of distances) and distances filename from terminal arguments
+    distances_filename = argv[1];
     dataset_filename = argv[2];
     
-    errors_file_descriptor = fopen(errors_filename, "r");
-    if (errors_file_descriptor == NULL){
-        perror("Error opening errors file:");
+    file_descriptor = fopen(distances_filename, "r");
+    if (file_descriptor == NULL){
+        perror("Error opening distances file:");
         exit(errno);
     }
     
     // Load dataset and hold number of time-series loaded
     num_ts = read_train_dataset(dataset_filename, &T);
-    printf("Number of time-series (and errors): %d\n", num_ts);
+    printf("Number of time-series (and distances): %d\n", num_ts);
     
-    // Load errors array from file
-    errors_array = safe_alloc(num_ts * sizeof(*errors_array));
+    // Load distances array from file
+    distances_array = safe_alloc(num_ts * sizeof(*distances_array));
     
     // Read all num_ts time-series from the dataset
     for(uint16_t i = 0; i < num_ts; i++){
         // Load HEX_BSIZE chars from file to buffer
-        if(!fgets(hex_buffer, HEX_BSIZE, errors_file_descriptor)){
-            perror("Error reading hex error: ");
+        if(!fgets(hex_buffer, HEX_BSIZE, file_descriptor)){
+            perror("Error reading hex distance: ");
             exit(errno);
         }
         
@@ -52,20 +52,23 @@ int main(int argc, char *argv[]){
             exit(-1);
         }
         
-        sscanf(hex_buffer, "%08x", &uint_error);
+        sscanf(hex_buffer, "%08x", &uint_distance);
         union{
             float f;
             uint32_t u;
         } f2u;
-        f2u.u = uint_error;
-        float_error = f2u.f;
+        f2u.u = uint_distance;
+        float_distance = f2u.f;
         
-        printf("%08x, %g\n", uint_error, float_error);
+        printf("%08x, %g\n", uint_distance, float_distance);
         
-        errors_array[i] = float_error;
+        distances_array[i] = float_distance;
     }
     
-    free(errors_array);
+    fstat = bin_f_statistic(distances_array, T, num_ts);
+    printf("F-statistic: %g\n", fstat);
+    
+    free(distances_array);
    
     return 0;
 }
