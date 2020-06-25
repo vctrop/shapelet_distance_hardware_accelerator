145d8bbef4abd4fc80e32011d74df21b46536f3b
Victor O. Costa <victor-o.c@hotmail.com>
2020-06-25 03:41:48 -0300

    Reorganize Flopoco script and include useHardMult=no for all cores, to prevent using FPGA hard multipliers and DSP blocks

--- hardware/flopoco/FPGen.sh
+++ hardware/flopoco/FPGen.sh
@@ -8,34 +8,36 @@ mkdir -p flopocoCores
 #sudo chown -R $USER flopocoCores/
 
 # --- Generates Cores ---
-
 # Single-Path FP Adder
-flopoco FPAdd wE=8 wF=23 dualPath=false TestBench n=20 file=false name=fp_adder_single
-mv flopoco.vhdl flopocoCores/fp_adder_single.vhd
-
-# Other Single-Path FP Adder
-flopoco FPAddSinglePath wE=8 wF=23 TestBench n=20 file=false name=fp_adder_single2
-mv flopoco.vhdl flopocoCores/fp_adder_single2.vhd
- 
-# Dual-Path FP Adder
-flopoco FPAdd wE=8 wF=23 dualPath=true TestBench n=20 file=false name=fp_adder_dual
-mv flopoco.vhdl flopocoCores/fp_adder_dual.vhd
- 
-# FP Multiplier 
-flopoco FPMult wE=8 wF=23 TestBench n=20 file=false name=fp_mult
-mv flopoco.vhdl flopocoCores/fp_mult.vhd
+flopoco FPAdd               n=20 wE=8 wF=23     file=false useHardMult=no TestBench dualPath=false name=fp_adder_single
+    
+# Other Single-Path FP Adder    
+flopoco FPAddSinglePath     n=20 wE=8 wF=23     file=false useHardMult=no TestBench name=fp_adder_single2 
+    
+# Dual-Path FP Adder            
+flopoco FPAdd               n=20 wE=8 wF=23     file=false useHardMult=no TestBench dualPath=true  name=fp_adder_dual
+    
+# FP Multiplier                 
+flopoco FPMult              n=20 wE=8 wF=23     file=false useHardMult=no TestBench name=fp_mult
  
 # FP Squarer
-flopoco FPSquare wE=8 wF_in=23 wF_out=23 TestBench n=20 file=false name=fp_power
-mv flopoco.vhdl flopocoCores/fp_power.vhd
- 
-# FP Divider
-flopoco FPDiv wE=8 wF=23 TestBench n=20 file=false name=fp_div
-mv flopoco.vhdl flopocoCores/fp_div.vhd
- 
-# FP Square Root
-flopoco FPSqrt wE=8 wF=23 TestBench n=20 file=false name=fp_sqrt
-mv flopoco.vhdl flopocoCores/fp_sqrt.vhd
+flopoco FPSquare            n=20 wE=8 wF_in=23  file=false useHardMult=no TestBench wF_out=23 name=fp_power
+
+# FP Divider                
+flopoco FPDiv               n=20 wE=8 wF=23     file=false useHardMult=no TestBench file=false name=fp_div
+    
+# FP Square Root                
+flopoco FPSqrt              n=20 wE=8 wF=23     file=false useHardMult=no TestBench name=fp_sqrt
+
+# Move files                
+mv flopoco.vhdl flopocoCores/fp_adder_single.vhd        # Single-Path FP Adder
+mv flopoco.vhdl flopocoCores/fp_adder_single2.vhd       # Other Single-Path FP Adder
+mv flopoco.vhdl flopocoCores/fp_adder_dual.vhd          # Dual-Path FP Adder
+mv flopoco.vhdl flopocoCores/fp_mult.vhd                # FP Multiplier     
+mv flopoco.vhdl flopocoCores/fp_power.vhd               # FP Squarer
+mv flopoco.vhdl flopocoCores/fp_div.vhd                 # FP Divider
+mv flopoco.vhdl flopocoCores/fp_sqrt.vhd                # FP Square Root
+
  
 # --- Generates Converters
 
