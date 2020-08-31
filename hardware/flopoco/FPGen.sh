3555ce90731190fff67c26fac39492d9f1a1d9da
Victor O. Costa <victor-o.c@hotmail.com>
2020-08-31 20:17:08 -0300

    Fix file moving in script that generates FloPoCo code

--- hardware/flopoco/FPGen.sh
+++ hardware/flopoco/FPGen.sh
@@ -9,34 +9,38 @@ mkdir -p flopocoCores
 
 # --- Generates Cores ---
 # Single-Path FP Adder
-flopoco FPAdd               n=20 wE=8 wF=23     file=false useHardMult=no TestBench dualPath=false name=fp_adder_single
-    
+flopoco FPAdd               n=20 wE=8 wF=23     frequency=300 file=false useHardMult=no TestBench dualPath=false name=fp_adder_single
+mv flopoco.vhdl flopocoCores/fp_adder_single.vhd        # Single-Path FP Adder    
+
 # Other Single-Path FP Adder    
-flopoco FPAddSinglePath     n=20 wE=8 wF=23     file=false useHardMult=no TestBench name=fp_adder_single2 
-    
+flopoco FPAddSinglePath     n=20 wE=8 wF=23     frequency=300 file=false useHardMult=no TestBench name=fp_adder_single2 
+mv flopoco.vhdl flopocoCores/fp_adder_single2.vhd       # Other Single-Path FP Adder
+
 # Dual-Path FP Adder            
-flopoco FPAdd               n=20 wE=8 wF=23     file=false useHardMult=no TestBench dualPath=true  name=fp_adder_dual
-    
+flopoco FPAdd               n=20 wE=8 wF=23     frequency=300 file=false useHardMult=no TestBench dualPath=true  name=fp_adder_dual
+mv flopoco.vhdl flopocoCores/fp_adder_dual.vhd          # Dual-Path FP Adder    
+
 # FP Multiplier                 
-flopoco FPMult              n=20 wE=8 wF=23     file=false useHardMult=no TestBench name=fp_mult
- 
+flopoco FPMult              n=20 wE=8 wF=23     frequency=300 file=false useHardMult=no TestBench name=fp_mult
+mv flopoco.vhdl flopocoCores/fp_mult.vhd                # FP Multiplier    
+
 # FP Squarer
-flopoco FPSquare            n=20 wE=8 wF_in=23  file=false useHardMult=no TestBench wF_out=23 name=fp_power
+flopoco FPSquare            n=20 wE=8 wF_in=23  frequency=300 file=false useHardMult=no TestBench wF_out=23 name=fp_power
+mv flopoco.vhdl flopocoCores/fp_power.vhd               # FP Squarer
 
 # FP Divider                
-flopoco FPDiv               n=20 wE=8 wF=23     file=false useHardMult=no TestBench file=false name=fp_div
-    
-# FP Square Root                
-flopoco FPSqrt              n=20 wE=8 wF=23     file=false useHardMult=no TestBench name=fp_sqrt
+flopoco FPDiv               n=20 wE=8 wF=23     frequency=300 file=false useHardMult=no TestBench file=false name=fp_div
+mv flopoco.vhdl flopocoCores/fp_div.vhd                 # FP Divider    
 
-# Move files                
-mv flopoco.vhdl flopocoCores/fp_adder_single.vhd        # Single-Path FP Adder
-mv flopoco.vhdl flopocoCores/fp_adder_single2.vhd       # Other Single-Path FP Adder
-mv flopoco.vhdl flopocoCores/fp_adder_dual.vhd          # Dual-Path FP Adder
-mv flopoco.vhdl flopocoCores/fp_mult.vhd                # FP Multiplier     
-mv flopoco.vhdl flopocoCores/fp_power.vhd               # FP Squarer
-mv flopoco.vhdl flopocoCores/fp_div.vhd                 # FP Divider
+# FP Square Root                
+flopoco FPSqrt              n=20 wE=8 wF=23     frequency=300 file=false useHardMult=no TestBench name=fp_sqrt
 mv flopoco.vhdl flopocoCores/fp_sqrt.vhd                # FP Square Root
+
+
+  
+
+
+
 
  
 # --- Generates Converters
